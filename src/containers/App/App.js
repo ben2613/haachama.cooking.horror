f584f6a9dcfc1be69b3496587c169dfa7dfd7b91
ben2613 <sosgyuki@gmail.com>
2021-02-25 22:38:06 +0800

    Remove the semicolon

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import logo from '../../logo.svg'
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          WIP for #はあちゃまホラー
-        </p>
-      </header>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
