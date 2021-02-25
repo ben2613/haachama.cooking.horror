209395ba924e7a71daa91480e71cd2bda2b3ef89
ben2613 <sosgyuki@gmail.com>
2021-02-26 02:22:36 +0800

    cover and paper4

--- src/containers/Book/Book.js
+++ src/containers/Book/Book.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import './Book.css'
 
 function Book(props) {
+  const [bookOpen, setBookOpen] = useState(false)
+  function handleClick() {
+    setBookOpen(true)
+  }
   return (
-    <div>
-      <div className="book">
-      <div className="back"></div>
-      <div className="page6"></div>
-      <div className="page5"></div>
-      <div className="page4"></div>
-      <div className="page3"></div>
-      <div className="page2"></div>
-      <div className="page1"></div>
-      <div className="front"></div>
-    </div>
+    <div className="bookContainer" onClick={handleClick}>
+      <div className={"book " + (bookOpen ? 'open' : '')}>
+        <div className="back"></div>
+        <div className="page6"></div>
+        <div className="page5"></div>
+        <div className="page4"></div>
+        <div className="page3"></div>
+        <div className="page2"></div>
+        <div className="page1"></div>
+        <div className="front"></div>
+      </div>
     </div>
   )
 }
