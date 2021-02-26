07965ce70ec21b84034204ea78b61508662aaf4f
ben2613 <sosgyuki@gmail.com>
2021-02-27 01:32:54 +0800

    Update words on HAACHAMA

---
+++ src/pages/WhatHaveYouCook.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function WhatHaveYouCook() {
+  return (
+    <div>
+      Haachama just made normal tomato pasta, ramen...<br />
+      Oh I sometimes add <span style={{color:"#fc723c"}}>CHICKEN</span>,
+      <span style={{color:"#5c4874"}}>TAKO</span> and
+      <span style={{color:"#166ca2"}}>SHARK</span>!
+      <p style={{fontSize:"3rem",lineHeight:"normal"}}>
+        NOBODY GOT HURT
+      </p>
+    </div>
+  )
+}
---
+++ src/pages/WhyAreYouHere.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function WhyAreYouHere() {
+  return (
+    <div>
+      Haachama was sealed in this book because my cooking is too delicious for human beings.
+      <p style={{fontSize:"3rem",lineHeight:"normal"}}>
+        HAACHAMA did NOTHING wrong
+      </p>
+    </div>
+  )
+}
