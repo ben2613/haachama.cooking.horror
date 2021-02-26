61fc51978619557d229b1b5c207eb6118fe00479
ben2613 <sosgyuki@gmail.com>
2021-02-26 21:48:29 +0800

    Add a function to activate pageflip

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,7 +2,7 @@ import { Container } from '@material-ui/core'
 import Phone from 'components/Phone/Phone'
 import Book from 'containers/Book/Book'
 import React, { useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { Link, Route, Switch, useLocation } from 'react-router-dom'
 import Ghost from '../../components/Ghost/Ghost'
 import Background from '../Background/Background'
 import './App.css'
@@ -10,17 +10,31 @@ import './App.css'
 function App() {
   const location = useLocation()
   const [bookOpen, setBookOpen] = useState(location.pathname !== '/')
-
+  const [bookFlip, setBookFlip] = useState(false)
+  function linkUpdateHandler() {
+    setBookFlip(true)
+    setTimeout(()=>{
+      setBookFlip(false)
+    }, 1000)
+  }
   return (
     <div className="App">
       <Background onClick={()=>setBookOpen(false)} />
       <Ghost />
       <Container>
-        <Book bookOpen={bookOpen} handleClick={()=>setBookOpen(true)} left={"ABCDEFTGH"}>
-          Test
+        <Book bookOpen={bookOpen} handleClick={()=>setBookOpen(true)} left={"ABCDEFTGH"} bookFlip={bookFlip} handleLinkUpdate={linkUpdateHandler}>
+          <Switch>
+            <Route path="/" exact={true}>
+              Test
+            </Route>
+            <Route path="/a/">
+              ABCDEFAAAAAA
+            </Route>
+          </Switch>
         </Book>
         <Phone />
       </Container>
+      <Link to="/a/">Test</Link>
     </div>
   )
 }
--- src/containers/Book/Book.js
+++ src/containers/Book/Book.js
@@ -1,14 +1,20 @@
-import { Box } from '@material-ui/core'
+import { Box, Button } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import React from 'react'
 import './Book.scss'
 
 
-function Book(props) {
+function Book({ bookOpen,
+  handleClick,
+  children,
+  left,
+  bookFlip,
+  handleLinkUpdate }) {
   return (
-    <div className="bookContainer" onClick={props.handleClick}>
-      <div className={"book " + (props.bookOpen ? 'open' : '')}>
+    <div className="bookContainer" onClick={handleClick}>
+      <div className={"book " + (bookOpen ? 'open' : '')}>
         <div className="back"></div>
+        <div className={"pageAnimation " + (bookFlip ? "active" : "")}></div>
         <div className="page6"></div>
         <div className="page5"></div>
         <div className="page4"></div>
@@ -16,19 +22,20 @@ function Book(props) {
         <div className="page2"></div>
         <div className="page1"></div>
         <div className="front">
-          <Box position="absolute" left="50%" top="50%" style={{transform:'translate(-50%, -50%)'}}>
-            <p style={{color:'rgb(138,3,3)', fontSize:'3rem'}}>
+          <Box position="absolute" left="50%" top="50%" style={{ transform: 'translate(-50%, -50%)' }}>
+            <p style={{ color: 'rgb(138,3,3)', fontSize: '3rem' }}>
               HAACHAMA
               COOKING
             </p>
             <span className="drop"></span>
           </Box>
         </div>
-        <Box className="content left" lineHeight="1.465em" textAlign="left" paddingLeft="1em" paddingTop="3em" visibility={props.bookOpen ? "visible" : "hidden"}>
-          {props.left}
+        <Box className="content left" lineHeight="1.465em" textAlign="left" paddingLeft="1em" paddingTop="3em" visibility={bookOpen ? "visible" : "hidden"}>
+          {left}
         </Box>
-        <Box className="content" lineHeight="1.465em" textAlign="left" paddingLeft="1em" paddingTop="3em" visibility={props.bookOpen ? "visible" : "hidden"}>
-          {props.children}
+        <Box className="content" lineHeight="1.465em" textAlign="left" paddingLeft="1em" paddingTop="3em" visibility={bookOpen ? "visible" : "hidden"}>
+          {children}
+          <Button onClick={handleLinkUpdate}>ABC</Button>
         </Box>
       </div>
     </div>
@@ -40,6 +47,8 @@ Book.propTypes = {
   handleClick: PropTypes.func,
   children: PropTypes.node,
   left: PropTypes.node,
+  bookFlip: PropTypes.bool,
+  handleLinkUpdate: PropTypes.func,
 }
 
 export default Book
