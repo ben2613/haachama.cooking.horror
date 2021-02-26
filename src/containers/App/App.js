4d2c40a0646354880a8b8e087dda169697e37f55
ben2613 <sosgyuki@gmail.com>
2021-02-26 17:53:16 +0800

    Adjustment

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,15 @@
 import { Container } from '@material-ui/core'
+import Phone from 'components/Phone/Phone'
 import Book from 'containers/Book/Book'
 import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import Ghost from '../../components/Ghost/Ghost'
 import Background from '../Background/Background'
 import './App.css'
 
 function App() {
-  const [bookOpen, setBookOpen] = useState(false)
+  const location = useLocation()
+  const [bookOpen, setBookOpen] = useState(location.pathname !== '/')
 
   return (
     <div className="App">
@@ -16,6 +19,7 @@ function App() {
         <Book bookOpen={bookOpen} handleClick={()=>setBookOpen(true)} left={"ABCDEFTGH"}>
           Test
         </Book>
+        <Phone />
       </Container>
     </div>
   )
