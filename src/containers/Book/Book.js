67c4c4ac2c953f664a6e18672c3026489eb6d285
ben2613 <sosgyuki@gmail.com>
2021-02-26 15:41:36 +0800

    Finish cover blood text

--- src/containers/Book/Book.js
+++ src/containers/Book/Book.js
@@ -1,7 +1,7 @@
 import { Box } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import React from 'react'
-import './Book.css'
+import './Book.scss'
 
 
 function Book(props) {
@@ -15,7 +15,15 @@ function Book(props) {
         <div className="page3"></div>
         <div className="page2"></div>
         <div className="page1"></div>
-        <div className="front"></div>
+        <div className="front">
+          <Box position="absolute" left="50%" top="50%" style={{transform:'translate(-50%, -50%)'}}>
+            <p style={{color:'rgb(138,3,3)', fontSize:'3rem'}}>
+              HAACHAMA
+              COOKING
+            </p>
+            <span className="drop"></span>
+          </Box>
+        </div>
         <Box className="content left" lineHeight="1.465em" textAlign="left" paddingLeft="1em" paddingTop="3em" visibility={props.bookOpen ? "visible" : "hidden"}>
           {props.left}
         </Box>
